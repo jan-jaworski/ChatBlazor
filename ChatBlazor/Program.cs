29d6c76ee0f5dbbeaab0b599916a6fa74418b74a
javam140 <javam140@gmail.com>
2024-12-15 15:59:54 +0100

    Add project files.

---
+++ ChatBlazor/Program.cs
@@ -0,0 +1,29 @@
+using ChatBlazor.Components;
+using ChatBlazor.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.MapHub<ChatHub>("/chathub");
+
+
+app.Run();
