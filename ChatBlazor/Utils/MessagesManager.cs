6838c755c7eb71b6d0a2806587879c635e6439de
Jan Jaworski <81302662+javam140@users.noreply.github.com>
2025-01-10 15:05:56 +0100

    MessagesManager - WIP

---
+++ ChatBlazor/Utils/MessagesManager.cs
@@ -0,0 +1,10 @@
+﻿using ChatBlazor.Models;
+using ChatBlazor.Contexts;
+
+namespace ChatBlazor.Utils
+{
+    public static class MessagesManager
+    {
+        //todo inject context some methods for adding chats msgs,  
+    }
+}
