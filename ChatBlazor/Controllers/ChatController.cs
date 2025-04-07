bd13a6053826193cd6ca8a68bf6bf340685a8ece
Jan Jaworski <javam140@gmail.com>
2025-04-07 19:21:04 +0200

    Endpoints for Mobile - WIP
    for now only login and register

---
+++ ChatBlazor/Controllers/ChatController.cs
@@ -0,0 +1,63 @@
+﻿using ChatBlazor.Utils;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+
+namespace ChatBlazor.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ChatController : ControllerBase
+{
+    private readonly UserManager<IdentityUser> _userManager;
+    private readonly SignInManager<IdentityUser> _signInManager;
+
+    public ChatController(
+        UserManager<IdentityUser> userManager,
+        SignInManager<IdentityUser> signInManager,
+        MessagesManager messagesManager)
+    {
+        _userManager = userManager;
+        _signInManager = signInManager;
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody] RegisterModel model)
+    {
+        var user = new IdentityUser { UserName = model.Username, Email = model.Email };
+        var result = await _userManager.CreateAsync(user, model.Password);
+        if (result.Succeeded)
+        {
+            return Ok(new { Message = "User registered successfully." });
+        }
+        return BadRequest(result.Errors);
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] LoginModel model)
+    {
+        var result = await _signInManager.PasswordSignInAsync(model.Username, model.Password, model.RememberMe, lockoutOnFailure: false);
+        if (result.Succeeded)
+        {
+            return Ok(new { Message = "User logged in successfully." });
+        }
+        return BadRequest(new { Message = "Invalid login attempt." });
+    }
+
+}
+
+
+public class RegisterModel
+{
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
+
+public class LoginModel
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public bool RememberMe { get; set; }
+}
