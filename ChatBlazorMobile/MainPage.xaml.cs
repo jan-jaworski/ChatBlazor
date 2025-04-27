3b7234176242cea798a5ecba20f75d4d3960684c
Jan Jaworski <javam140@gmail.com>
2025-04-27 20:02:59 +0200

    Mobile Register Page - frontend

--- ChatBlazorMobile/MainPage.xaml.cs
+++ ChatBlazorMobile/MainPage.xaml.cs
@@ -6,4 +6,9 @@ public partial class MainPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void registerButton_Clicked(object sender, EventArgs e)
+    {
+		Navigation.PushAsync(new RegisterPage());
+    }
 }
